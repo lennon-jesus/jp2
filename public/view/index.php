91c030bddaf809d8086457ed08328d8284c34bec
lennon-jesus <161793432+lennon-jesus@users.noreply.github.com>
2025-05-06 17:02:00 -0300

    Add files via upload

---
+++ public/view/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=logout" />
+  <link href="../css/style.css" rel="stylesheet">
+  <title>Início - Hotel Winner</title>
+</head>
+
+<body>
+  <?php include('navbar.php'); ?>
+  <img src="../img/winner.png" alt="Hotel Winner" style="margin: auto; margin-top: 3rem; display: block">
+  <h1 style="margin:auto; text-align:center;"> Bem-vindo(a)! </h1>
+  <div class="card" style="display: block; margin: auto; margin-top: 3em; width: 50em;">
+    <div class="card-body" style="margin:auto; text-align: center">
+      <a href="atendimento.php" class="btn btn-light btn-lg" role="button">Atendimento</a>
+      <a href="#" class="btn btn-light btn-lg" role="button">Gerenciamento</a>
+      <a href="#" class="btn btn-light btn-lg" role="button">Relatórios</a>
+      <a href="logout.php" class="btn btn-light btn-lg" role="button"><span
+          class="material-symbols-outlined">logout</span> Desconectar</a>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
