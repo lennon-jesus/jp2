7e58dcc4e95d9804a7533426afe254b7a13f7290
lennon-jesus <161793432+lennon-jesus@users.noreply.github.com>
2025-05-06 17:36:42 -0300

    Update atendimentoController.php

--- public/controller/atendimentoController.php
+++ public/controller/atendimentoController.php
@@ -8,28 +8,5 @@ $rooms = $roomsExecute->fetchAll(PDO::FETCH_ASSOC);
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (
-    isset($data['nome']) && 
-    isset($data['nascimento']) && 
-    isset($data['fiador']) && 
-    isset($data['contato'])
-) {
-    $nome = $data['nome'];
-    $nascimento = $data['nascimento'];
-    $fiador = $data['fiador'];
-    $contato = $data['contato'];
-
-    $sql = "INSERT INTO hospedagem (nome, nascimento, fiador, contato) VALUES (?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssss", $nome, $nascimento, $fiador, $contato);
-
-    if ($stmt->execute()) {
-        echo json_encode(["success" => true]);
-    } else {
-        echo json_encode(["success" => false, "error" => $stmt->error]);
-    }
-} else {
-    echo json_encode(["success" => false, "error" => "Dados incompletos"]);
-}
 
 ?>
